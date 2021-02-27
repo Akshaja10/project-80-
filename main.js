0893cb38f0ce40a72de4f402e444337a1752e179
Akshaja10 <76399647+Akshaja10@users.noreply.github.com>
2021-02-27 10:12:31 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+function showParagraph1() 
+{
+var inputs=[];
+for (var i =1 ; i<=6; i++)
+{
+inputs.push (document.getElementById ("div1_input_box_"+ i).value);
+}
+document.getElementById ("showParagraph1").innerHTML=inputs.join(". ");
+
+
+}
+function showParagraph2() 
+{
+var inputs=[];
+for (var i =1 ; i<=6; i++)
+{
+inputs.push (document.getElementById ("para1_input_box_"+ i).value);
+
+}
+document.getElementById ("showParagraph2").innerHTML=inputs.join(". ");
+
+
+}
